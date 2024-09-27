e16de598f64291050ab25f6511a5b1a52b03cc31
David James <davidcjames@gmail.com>
2024-09-26 22:17:42 -0400

    fix: another unsafe conversion in rel_inner_pos

--- src/crc_store/seek.rs
+++ src/crc_store/seek.rs
@@ -77,7 +77,7 @@ impl<I: Read + Write + Seek> CrcStore<I> {
     ) -> Result<i64, TryFromIntError> {
         let b = i64::try_from(self.body_len())?;
         let s = self.seg_len as u64;
-        let offset = (inner_pos % s) as i64;
+        let offset = i64::try_from(inner_pos % s)?;
         assert!(offset >= 4);
         let body_offset = offset - 4;
         let shift = if rel_outer_pos >= 0 {
