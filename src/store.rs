7202c33bc5b7b9d4505a01b98a1a982e9b7e815e
David James <davidcjames@gmail.com>
2024-10-07 11:22:30 -0400

    fix: disallow certain inner lengths for `new` #7
    This fixes issue #7.

--- src/store.rs
+++ src/store.rs
@@ -61,12 +61,51 @@ impl<I: Read + Write + Seek> CrcStore<I> {
 
     /// Returns a new `CrcStore`. Seeks to the first segment's first body byte
     /// (even if this byte doesn't exist yet), in the inner I/O object.
+    ///
+    /// This method does not call `validate()` on the inner I/O object; it does
+    /// not inspect the checksums.
+    ///
+    /// ## Disallowed Lengths
+    ///
+    /// Even though the checksums of the inner I/O object is not validated,
+    /// there are some disallowed lengths which will return an error. These
+    /// disallowed lengths can be calculated using a simple formula: if the last
+    /// segment is a partial segment, it must have length 5 or greater.
+    ///
+    /// ### Example: `seg_len == 8`
+    ///
+    /// For `seg_len == 8`, here are the valid inner lengths and corresponding
+    /// body lengths:
+    ///
+    /// ```text
+    /// ║     inner_len     ║    body length    ║
+    /// ╠════╤════╤════╤════╬════╤════╤════╤════╣
+    /// ║  5 │  6 │  7 │  8 ║  1 │  2 │  3 │  4 ║
+    /// ║ 13 │ 14 │ 15 │ 16 ║  5 │  6 │  7 │  8 ║
+    /// ║ 21 │ 22 │ 23 │ 24 ║  9 │ 10 │ 11 │ 12 ║
+    /// ```
+    ///
+    /// ### Example: `seg_len == 16`
+    ///
+    /// For `seg_len == 16, here are the valid inner lengths:
+    /// - 5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16
+    /// - 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+    /// - 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48
+    ///
+    /// ... and corresponding body lengths:
+    /// - 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12
+    /// - 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
+    /// - 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
     pub fn new(config: Config, mut inner: I) -> Result<Self, Error> {
         config.validate()?;
         let inner_len = inner.seek(SeekFrom::End(0))?;
-        if inner_len > 0 && inner_len < 5 {
+
+        // Handle disallowed lengths by returning an error
+        let offset = inner_len % config.seg_len as u64;
+        if offset > 0 && offset < 5 {
             return Err(Error::BadInnerLen);
         }
+
         let inner_pos = inner.seek(SeekFrom::Start(0))?;
         Ok(Self {
             cfg: config,
